16eecebd8b5e101ea94839c6b784c1bf8b1587b2
Tarun Sharma <sharmatarun1019@gmail.com>
2025-01-09 14:54:18 +0530

    Revert "fix: error"
    This reverts commit c098e0611c2edd9fe6226778d9c45b9e4c36e0b3.

--- components/ui/chart.tsx
+++ components/ui/chart.tsx
@@ -81,18 +81,18 @@ const ChartStyle = ({ id, config }: { id: string; config: ChartConfig }) => {
       dangerouslySetInnerHTML={{
         __html: Object.entries(THEMES).map(
           ([theme, prefix]) => `
-            ${prefix} [data-chart=${id}] {
-            ${colorConfig
-              .map(([key, itemConfig]) => {
-                const color =
-                  itemConfig.theme?.[theme as keyof typeof itemConfig.theme] ||
-                  itemConfig.color;
-                return color ? `  --color-${key}: ${color};` : null;
-              })
-              .join("\n")}
-            }
-            `,
-        )[0],
+${prefix} [data-chart=${id}] {
+${colorConfig
+  .map(([key, itemConfig]) => {
+    const color =
+      itemConfig.theme?.[theme as keyof typeof itemConfig.theme] ||
+      itemConfig.color;
+    return color ? `  --color-${key}: ${color};` : null;
+  })
+  .join("\n")}
+}
+`,
+        ),
       }}
     />
   );
